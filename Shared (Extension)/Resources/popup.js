4f5aeb45fa015f751a26c6e3ac17f182b10609f4
OlegWock <olegwock@gmail.com>
2023-07-31 20:58:37 +0200

    Beep

--- Shared (Extension)/Resources/popup.js
+++ Shared (Extension)/Resources/popup.js
@@ -1,1 +1,16 @@
-console.log("Hello World!", browser);
+const loadPermissions = () => {
+    browser.permissions.getAll(res => {
+        document.querySelector('#host_permissions').textContent = res.origins.join(', ');
+        document.querySelector('#api_permissions').textContent = res.permissions.join(', ');
+    });
+};
+
+loadPermissions();
+
+document.querySelector('#request_tabs').addEventListener('click', () => {
+    browser.permissions.request({permissions: ["tabs"]}, loadPermissions);
+});
+
+document.querySelector('#request_host').addEventListener('click', () => {
+    browser.permissions.request({origins: ["*://example.com/"]}, loadPermissions);
+});
